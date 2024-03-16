cbf6cda912f0af7d60e77eadd5c6ef7faa0a9663
sadekcric4 <sadekcric4@gmail.com>
2024-03-16 13:28:30 +0600

    all updated done

--- src/Components/OurRecipes/CartProduct/Table2.jsx
+++ src/Components/OurRecipes/CartProduct/Table2.jsx
@@ -3,7 +3,7 @@
 const Table2 = ({cooking, index}) => {
   return (
   
-    <tr>
+    <tr className='bg-gray-100'>
       <td className='p-3'>{index+1}</td>
       <td className='p-3'>{cooking['recipe_name']}</td>
       <td className='p-3'>{cooking['preparing_time']} minutes</td>
